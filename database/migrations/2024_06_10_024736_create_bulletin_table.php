fbacdbe6f51ce7a24f909a5ff88353fe6679c645
atikaelya <136929632+atikaelya@users.noreply.github.com>
2024-06-10 11:04:13 +0800

    update migrations

---
+++ database/migrations/2024_06_10_024736_create_bulletin_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bulletin', function (Blueprint $table) {
+            $table->id('bulletinId');
+            $table->string('bulletinTitle');
+            $table->date('publishDate');
+            $table->string('bulletinDetails');
+            $table->integer('createdBy');
+            $table->datetime('createdAt');
+            $table->datetime('updatedAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bulletin');
+    }
+};
---
+++ database/migrations/2024_06_10_025915_create_publish_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publish', function (Blueprint $table) {
+            $table->id(publishId);
+            $table->id(bulletin_bulletinId);
+            $table->integer(publishTo);
+            $table->datetime(createdAt);
+            $table->datetime(updatedAt);   
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publish');
+    }
+};
